c833aabc26cf68e68c7a9796c2987a57b233d24c
pntech20 <pntech20@gmail.com>
2022-05-27 07:38:43 +0700

    google, UI

---
+++ app/src/comps/register/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./styles.scss";
+import IconNear from '../icons/near'
+import ReCAPTCHA from "react-google-recaptcha";
+import GoogleLogin from 'react-google-login';
+
+
+
+export default function Register() {
+  const recaptchaRef = React.createRef();
+  const [isReCaptchaLoaded, setIsReCaptchaLoaded] = useState(false)
+
+  const onSubmitWithReCAPTCHA = async (e) => {
+    e.preventDefault()
+    if (isReCaptchaLoaded) {
+    }
+  }
+  const recaptchaLoaded = () => {
+    setIsReCaptchaLoaded(true)
+  }
+  const responseGoogle = () => {
+
+  }
+  return (
+    <div className="wrap">
+      <h3>REGISTER</h3>
+      <form onSubmit={onSubmitWithReCAPTCHA}>
+        <div className="flex">
+          <div className="left" >
+            <div>
+              <label>First name</label>
+              <input />
+            </div>
+            <div>
+              <label>Last name</label>
+              <input />
+            </div>
+            <div>
+              <label>Email</label>
+              <input />
+            </div>
+            <div>
+              <label>Create Password</label>
+              <input />
+            </div>
+            <div>
+              <label>Confirm Password</label>
+              <input />
+            </div>
+            <button className="btn-register">REGISTER</button>
+            <div className="by-clicking"><p>By clicking 'Register', I agree to the</p>
+              <a href="/"> Terms of Use</a> and <a href="/">Privacy Policy</a></div>
+            <div className="divider"></div>
+            <p className="by-clicking already-registered">Already Registered? <a href="/">LOGIN</a></p>
+          </div>
+          <div className="center"></div>
+          <div className="right">
+            <div>
+              <p className="fw-bold">Connect with below options</p>
+              <div className="ctn-near-wallet">
+                <div className="ctn-icon">
+                  <span><IconNear /></span>
+                </div>
+                <span>NEAR Wallet</span>
+              </div>
+              <div></div>
+              <div className="ctn-google">
+                <GoogleLogin buttonText="Google" clientId={process.env.REACT_APP_CLIENT_ID} onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy={'single_host_origin'}
+                />
+              </div>
+              <div className="ctn-recaptcha">
+                <p>Please check the box bellow to proceed</p>
+                <ReCAPTCHA
+                  size="normal"
+                  ref={recaptchaRef}
+                  sitekey={process.env.REACT_APP_RECAPTCHA_SITE_ID}
+                  onChange={recaptchaLoaded}
+                />
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
